a4cfd469e24af0c8dde952df90f45dfa99495a43
Jimmy <veniveci@aliyun.com>
2021-09-26 11:00:09 +0800

    fix: fix build error

--- ios/ReactNativeCameraKit/AliAVEngine/AliyunEditZoneView.m
+++ ios/ReactNativeCameraKit/AliAVEngine/AliyunEditZoneView.m
@@ -8,17 +8,9 @@
 
 #import "AliyunEditZoneView.h"
 
-
 @implementation AliyunEditZoneView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
-- (id)initWithFrame:(CGRect)frame
+- (instancetype)initWithFrame:(CGRect)frame
 {
     if (self = [super initWithFrame:frame]) {
         self.backgroundColor = [UIColor clearColor];
@@ -31,31 +23,37 @@
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     [super touchesBegan:touches withEvent:event];
-    if (_editStatus) {
-        CGPoint preLocation = [[touches anyObject] previousLocationInView:self];
-        [self.delegate currentTouchPoint:preLocation];
+    if (!_editStatus) {
+        return;
+    }
+    CGPoint previousPoint = [[touches anyObject] previousLocationInView:self];
+    if (self.delegate && [self.delegate respondsToSelector:@selector(currentTouchPoint:)]) {
+        [self.delegate currentTouchPoint:previousPoint];
     }
 }
 
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     [super touchesMoved:touches withEvent:event];
-    if (_editStatus) {
-        UITouch *touch = (UITouch *)[touches anyObject];
-        CGPoint fp = [touch previousLocationInView:self];
-        CGPoint tp = [touch locationInView:self];
-        //    [_currentPasterView touchMoveFromPoint:fp to:tp];
-        [self.delegate mv:fp to:tp];
+    if (!_editStatus) {
+        return;
+    }
+    UITouch *touch = (UITouch *)[touches anyObject];
+    CGPoint previousPoint = [touch previousLocationInView:self];
+    CGPoint touchPoint = [touch locationInView:self];
+    if (self.delegate && [self.delegate respondsToSelector:@selector(mv:to:)]) {
+        [self.delegate mv:previousPoint to:touchPoint];
     }
 }
 
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     [super touchesEnded:touches withEvent:event];
-    if (_editStatus) {
+    if (!_editStatus) {
+        return;
+    }
+    if (self.delegate && [self.delegate respondsToSelector:@selector(touchEnd)]) {
         [self.delegate touchEnd];
-        //    [_currentPasterView touchEnd];
-        //    _currentPasterView = nil;
     }
 }
 
