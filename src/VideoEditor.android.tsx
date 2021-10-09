69db645b2c68917386b6eb0180a5625c4962264e
wuyq <wuyq08@icloud.com>
2021-10-09 17:08:45 +0800

    android editor

---
+++ src/VideoEditor.android.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { requireNativeComponent } from 'react-native';
+import PropTypes from 'prop-types';
+const EditView = requireNativeComponent('RNEditView');
+
+class VideoEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.editRef = React.createRef();
+  }
+  _onExportVideo = (event) => {
+    if (!this.props.onExportVideo) {
+      return;
+    }
+    this.props.onExportVideo(event.nativeEvent);
+  };
+
+  render() {
+    return (
+      <EditView
+        ref={this.editRef}
+        style={{ minWidth: 100, minHeight: 100 }}
+        {...this.props}
+        onExportVideo={this._onExportVideo}
+      ></EditView>
+    );
+  }
+}
+
+VideoEditor.propTypes = {
+  onExportVideo: PropTypes.func,
+};
+
+export default VideoEditor;
