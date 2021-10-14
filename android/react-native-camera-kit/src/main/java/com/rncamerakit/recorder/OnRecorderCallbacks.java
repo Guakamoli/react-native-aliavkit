29b084838b75548f194eb19a22807d1a71c67572
wuyq <wuyq08@icloud.com>
2021-10-14 16:17:21 +0800

    删除多余文件

--- android/react-native-camera-kit/src/main/java/com/rncamerakit/recorder/OnRecorderCallbacks.java
+++
@@ -1,9 +0,0 @@
-package com.rncamerakit.recorder;
-
-public interface OnRecorderCallbacks {
-    default void  onTakePhoto(String photoPath){}
-    default void  onProgress(long duration){}
-    default void  onComplete(boolean validClip, long clipDuration){}
-    default void  onFinish(String outputPath){}
-    default void  onError(int errorCode){}
-}
