8127df5c6e32fbab38fc1d2180b3840c4f82d764
Jimmy <veniveci@aliyun.com>
2021-10-18 14:46:16 +0800

    feat: crop function

--- ios/ReactNativeCameraKit/Bridges/AliAVServiceBridge.m
+++ ios/ReactNativeCameraKit/Bridges/AliAVServiceBridge.m
@@ -10,6 +10,8 @@
 #import <AliyunVideoSDKPro/AliyunHttpClient.h>
 #import <Photos/Photos.h>
 #import "AVAsset+VideoInfo.h"
+#import <AVFoundation/AVFoundation.h>
+#import "AliyunPathManager.h"
 
 static NSString * const kAlivcQuUrlString =  @"https://alivc-demo.aliyuncs.com";
 
@@ -21,7 +23,9 @@ static NSString * const kAlivcQuUrlString =  @"https://alivc-demo.aliyuncs.com";
 
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(saveResourceToPhotoLibrary:(NSDictionary*)options resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+RCT_EXPORT_METHOD(saveResourceToPhotoLibrary:(NSDictionary*)options
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
 {
     NSString *sourcePath = [options valueForKey:@"sourcePath"];
     if (!sourcePath || [sourcePath isEqualToString:@""]) {
@@ -132,6 +136,245 @@ RCT_EXPORT_METHOD(getFacePasterInfos:(NSDictionary*)options
     } else {
         completion(false);
     }
+}
+
+RCT_EXPORT_METHOD(crop:(NSDictionary *)options
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+{
+    NSString *source = [options valueForKey:@"source"];
+    if (!source || ![source containsString:@"ph://"]) {
+        reject(@"",@"source scheme no ph://",nil);
+    }
+    CGFloat cropOffsetX = [[options valueForKey:@"cropOffsetX"] floatValue];
+    CGFloat cropOffsetY = [[options valueForKey:@"cropOffsetY"] floatValue];
+    CGFloat cropWidth = [[options valueForKey:@"cropWidth"] floatValue];
+    CGFloat cropHeight = [[options valueForKey:@"cropHeight"] floatValue];
+    
+    NSString *quality = [options valueForKey:@"quality"] ? : @"";
+    
+    if (cropWidth == 0.0 ) {
+        reject(@"",@"Invalid cropWidth",nil);
+    }
+    
+    if ( cropHeight == 0.0 ) {
+        reject(@"",@"Invalid cropHeight",nil);
+    }
+    
+    //save resource to sandbox from photo library
+    NSString *path = [NSURL URLWithString:source].path;
+    PHAsset *phAsset = [[PHAsset fetchAssetsWithLocalIdentifiers:@[path] options:nil] firstObject];
+    UIImage *phImage = [AliAVServiceBridge _getImageForAsset:phAsset withSize:CGSizeMake(phAsset.pixelWidth, phAsset.pixelHeight)];
+    NSString *imageUri = [[AliyunPathManager createResourceDir] stringByAppendingPathComponent:[AliyunPathManager randomString]];
+    NSData *imageData = UIImageJPEGRepresentation(phImage, 1.0 / sqrt(2.0));
+    [imageData writeToFile:imageUri atomically:YES];
+
+    NSURL *sourceURL = [NSURL URLWithString:imageUri];
+    
+    AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:sourceURL options:nil];
+    
+    NSString *outputURL = [[AliyunPathManager createResourceDir] stringByAppendingPathComponent:[AliyunPathManager randomString]];
+    
+    NSString *useQuality = [self _getQualityForAsset:quality asset:asset];
+    
+    AVAssetExportSession *exportSession = [[AVAssetExportSession alloc] initWithAsset:asset presetName:useQuality];
+    if (!exportSession) {
+        reject(@"",@"Error creating AVAssetExportSession",nil);
+    }
+    exportSession.outputURL = [NSURL fileURLWithPath:outputURL];
+    exportSession.outputFileType = AVFileTypeMPEG4;
+    exportSession.shouldOptimizeForNetworkUse = YES;
+    
+    AVMutableVideoComposition *videoComposition = [AVMutableVideoComposition videoCompositionWithPropertiesOfAsset:asset];
+    AVAssetTrack *clipVideoTrack = [[asset tracksWithMediaType:AVMediaTypeVideo] firstObject];
+    
+    UIImageOrientation videoOrientation = [self _getVideoOrientationFromAsset:asset];
+    
+    CGFloat videoWidth = 0.0;
+    CGFloat videoHeight = 0.0;
+    
+    if (videoOrientation == UIImageOrientationUp || videoOrientation == UIImageOrientationDown) {
+        videoWidth = clipVideoTrack.naturalSize.height;
+        videoHeight = clipVideoTrack.naturalSize.width;
+    } else {
+        videoWidth = clipVideoTrack.naturalSize.width;
+        videoHeight = clipVideoTrack.naturalSize.height;
+    }
+    
+    videoComposition.frameDuration = CMTimeMake(1, 30);
+    while (fmod(cropWidth, 2.0) > 0 && cropWidth < videoWidth) {
+        cropWidth += 1.0;
+    }
+    while (fmod(cropWidth, 2.0) > 0 && cropWidth > 0.0) {
+        cropWidth -= 1.0;
+    }
+    while (fmod(cropHeight, 2.0) > 0 && cropHeight < videoHeight) {
+        cropHeight += 1.0;
+    }
+    while (fmod(cropHeight, 2.0) > 0 && cropHeight > 0.0) {
+        cropHeight -= 1.0;
+    }
+    
+    videoComposition.renderSize = CGSizeMake(cropWidth, cropHeight);
+    
+    AVMutableVideoCompositionInstruction *instruction = [AVMutableVideoCompositionInstruction new];
+    instruction.timeRange = CMTimeRangeMake(kCMTimeZero, asset.duration);
+    
+    CGAffineTransform t1 = CGAffineTransformIdentity;
+    CGAffineTransform t2 = CGAffineTransformIdentity;
+    
+    AVMutableVideoCompositionLayerInstruction *transformer =
+    [AVMutableVideoCompositionLayerInstruction videoCompositionLayerInstructionWithAssetTrack:clipVideoTrack];
+    
+    switch (videoOrientation) {
+        case UIImageOrientationUp:
+            t1 = CGAffineTransformMakeTranslation(clipVideoTrack.naturalSize.height - cropOffsetX,
+                                                  0-cropOffsetY);
+            t2 = CGAffineTransformRotate(t1, M_PI_2);
+            break;
+        case UIImageOrientationLeft:
+            t1 = CGAffineTransformMakeTranslation(clipVideoTrack.naturalSize.width - cropOffsetX,
+                                                  clipVideoTrack.naturalSize.height - cropOffsetY);
+            t2 = CGAffineTransformRotate(t1, M_PI);
+            break;
+        case UIImageOrientationRight:
+            t1 = CGAffineTransformMakeTranslation(0 - cropOffsetX, 0-cropOffsetY);
+            t2 = CGAffineTransformRotate(t1, 0);
+            break;
+        case UIImageOrientationDown:
+            t1 = CGAffineTransformMakeTranslation(0 - cropOffsetX, clipVideoTrack.naturalSize.width - cropOffsetY);
+            t2 = CGAffineTransformRotate(t1, -M_PI_2);
+            break;
+            
+        default:
+            break;
+    }
+    
+    CGAffineTransform finalTransform = t2;
+    [transformer setTransform:finalTransform atTime:kCMTimeZero];
+    
+    instruction.layerInstructions = @[transformer];
+    videoComposition.instructions = @[instruction];
+    
+    exportSession.videoComposition = videoComposition;
+    
+    [exportSession exportAsynchronouslyWithCompletionHandler:^{
+        if (exportSession.status == AVAssetExportSessionStatusCompleted) {
+            resolve(outputURL);
+        }
+        else if (exportSession.status == AVAssetExportSessionStatusFailed) {
+            reject(@"",@"Failed",[exportSession error]);
+        }
+        else if (exportSession.status == AVAssetExportSessionStatusCancelled) {
+            reject(@"",@"Cancelled",[exportSession error]);
+        }
+    }];
+}
+
+- (NSURL *)_getSourceURL:(NSString *)source
+{
+    NSURL *sourceURL = nil;
+    if ([source containsString:@"assets-library"]) {
+        sourceURL = [NSURL URLWithString:source];
+    } else {
+        NSURL *bundleURL = [[NSBundle mainBundle] resourceURL];
+        sourceURL = [NSURL URLWithString:source relativeToURL:bundleURL];
+    }
+    
+    return sourceURL;
+}
+
+- (NSString *)_getQualityForAsset:(NSString *)quality asset:(AVAsset *)asset
+{
+    NSString *useQuality = AVAssetExportPresetPassthrough;
+    if ([quality containsString:@"low"]) {
+        useQuality = AVAssetExportPresetLowQuality;
+    }
+    else if ([quality containsString:@"medium"]) {
+        useQuality = AVAssetExportPresetMediumQuality;
+    }
+    else if ([quality containsString:@"highest"]) {
+        useQuality = AVAssetExportPresetHighestQuality;
+    }
+    else if ([quality containsString:@"640x480"]) {
+        useQuality = AVAssetExportPreset640x480;
+    }
+    else if ([quality containsString:@"960x540"]) {
+        useQuality = AVAssetExportPreset960x540;
+    }
+    else if ([quality containsString:@"1280x720"]) {
+        useQuality = AVAssetExportPreset1280x720;
+    }
+    else if ([quality containsString:@"1920x1080"]) {
+        useQuality = AVAssetExportPreset1920x1080;
+    }
+    
+    NSArray<NSString *> *arrary = [AVAssetExportSession exportPresetsCompatibleWithAsset:asset];
+    if (![arrary containsObject:useQuality]) {
+        useQuality = AVAssetExportPresetPassthrough;
+    }
+    return useQuality;
+}
+
+- (UIImageOrientation)_getVideoOrientationFromAsset:(AVAsset *)asset
+{
+    AVAssetTrack *videoTrack = [[asset tracksWithMediaType:AVMediaTypeVideo] firstObject];
+    CGSize size = videoTrack.naturalSize;
+    
+    CGAffineTransform txf = videoTrack.preferredTransform;
+    
+    if (size.width == txf.tx && size.height == txf.ty) {
+        return UIImageOrientationLeft;
+    }
+    else if (txf.tx == 0 && txf.ty == 0) {
+        return UIImageOrientationRight;
+    }
+    else if (txf.tx == 0 && txf.ty == size.width) {
+        return UIImageOrientationDown;
+    }
+    else {
+        return  UIImageOrientationUp;
+    }
+    
+}
+
++ (BOOL)_saveImage:(UIImage *)image toFileName:(NSString *)fileName
+{
+    NSError *error;
+    NSFileManager *fileMgr = [NSFileManager defaultManager];
+    NSString *documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    NSArray<NSString *> *directoryFiles = [fileMgr contentsOfDirectoryAtPath:documentsDirectory error:&error];
+    
+    if (error == nil && ![directoryFiles containsObject:fileName]) {
+        NSString *imageUri = [documentsDirectory stringByAppendingPathComponent:fileName];
+        NSData *imageData = UIImageJPEGRepresentation(image, 1.0 / sqrt(2.0));
+    
+        [imageData writeToFile:imageUri atomically:YES];
+        
+        return YES;
+    }
+    
+    return NO;
+}
+
++ (UIImage *)_getImageForAsset:(PHAsset *)asset withSize:(CGSize)size
+{
+    PHImageManager *phManager = [PHImageManager defaultManager];
+    __block UIImage *img;
+    
+    // Requesting image for asset
+    PHImageRequestOptions *requestOptions = [PHImageRequestOptions new];
+    requestOptions.synchronous = YES;
+
+    [phManager requestImageForAsset:asset
+                         targetSize:size
+                        contentMode:PHImageContentModeAspectFill
+                            options:requestOptions
+                      resultHandler:^void(UIImage *image, NSDictionary *info) {
+        img = image;
+    }];
+    
+    return img;
 }
 
 @end
