1a7ab198c76f1a56d5cfafdd2366c0875d3777f2
xujalg <1027754098@qq.com>
2021-11-09 11:21:56 +0800

    例子

--- images/index.js
+++ images/index.js
@@ -1,7 +1,40 @@
 const ImageMap = {
+
+
+
+  cameraFlipPng: require('./cameraFlipIcon.png'),
+  captureButtonPng: require('./cameraButton.png'),
+  closePng: require('./close.png'),
+  musicPng: require('./music.png'),
+  beautifyPng: require('./beautify.png'),
+  beautyAdjustPng: require('./beautyAdjust.png'),
+  AaPng: require('./Aa.png'),
+  filterPng: require('./filter.png'),
+  musicRevampPng: require('./musicRevamp.png'),
+  giveUpPng: require('./giveUp.png'),
+  noVolumePng: require('./noVolume.png'),
+  tailorPng: require('./tailor.png'),
+  volumePng: require('./volume.png'),
+  multipleBtnPng: require('./multipleBtn.png'),
+  startMultipleBtnPng: require('./startMultipleBtn.png'),
+  postCameraPng: require('./postCamera.png'),
+  changeSizePng: require('./changeSize.png'),
+
+
+
+
+
+
+  musicDynamicGif: require('./musicDynamic.gif'),
+  musicIconPng: require('./musicIcon.png'),
+  musicIcongray: require('./musicIcongray.png'),
+
+  musicSearchPng: require('./musicSearch.png'),
+  // selectBeautifyPng: require('./selectBeautify.png'),
+  // noResultPng: require('./noResult.png'),
+  videomusicIconPng: require('./videomusicIcon.png'),
+
   musicSelect: require('./musicSelect.png'),
   useMusic: require('./useMusic.png'),
 }
-
-
 export default ImageMap
