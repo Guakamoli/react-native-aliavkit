8b72b3a1446d5b2728728308a0cf5cb00eedd019
wuyq <wuyq08@icloud.com>
2021-11-30 13:53:13 +0800

    android playMusic

--- src/StoryEditor.tsx
+++ src/StoryEditor.tsx
@@ -122,7 +122,6 @@ export default class StoryEditor extends Component<Props, State> {
   }
   async pauseMusic(song) {
     console.info('暂停音乐', song);
-    console.info('暂停音乐', song.songID);
     await AVService.pauseMusic(song.songID);
   }
   //  发布快拍   导出视频  丢出数据
--- src/StoryEditorTest.tsx
+++ src/StoryEditorTest.tsx
@@ -114,7 +114,6 @@ export default class StoryEditorTest extends Component<Props, State> {
   }
   async pauseMusic(song) {
     console.info('暂停音乐', song);
-    console.info('暂停音乐test', song.songID);
     await AVService.pauseMusic(song.songID);
   }
   //  发布快拍   导出视频  丢出数据
