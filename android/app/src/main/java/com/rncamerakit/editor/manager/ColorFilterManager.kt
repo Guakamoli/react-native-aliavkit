bedb79b003e2b78ef9fe0634f62fd5c2a108c5a7
wuyq <wuyq08@icloud.com>
2022-03-15 00:49:37 +0800

    安卓端实现录制设置滤镜！

--- android/app/src/main/java/com/rncamerakit/editor/manager/ColorFilterManager.kt
+++ android/app/src/main/java/com/rncamerakit/editor/manager/ColorFilterManager.kt
@@ -1,11 +1,15 @@
 package com.rncamerakit.editor.manager
 
 import android.content.Context
+import android.util.Log
 import com.aliyun.svideo.editor.util.AlivcResUtil
 import com.aliyun.svideo.editor.util.EditorCommon
+import com.aliyun.svideo.recorder.util.RecordCommon
 import com.aliyun.svideosdk.common.struct.effect.EffectBean
+import com.aliyun.svideosdk.common.struct.effect.EffectFilter
 import com.aliyun.svideosdk.common.struct.project.Source
 import com.aliyun.svideosdk.editor.AliyunIEditor
+import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.Promise
 import com.facebook.react.uimanager.ThemedReactContext
 import com.google.gson.GsonBuilder
@@ -19,18 +23,32 @@ class ColorFilterManager(private val reactContext: ThemedReactContext) {
     companion object {
         fun getColorFilter(mContext: Context, promise: Promise) {
             val mColorFilterList: MutableList<ColorFilter> = ArrayList()
-            mColorFilterList.add(ColorFilter("无效果", "ic_color_filter_empty"))
+            mColorFilterList.add(ColorFilter("无效果", "ic_color_filter_empty", ""))
             EditorCommon.getColorFilterList(mContext).forEach { path ->
                 val name = File(path).name
                 val icon = "file://$path/icon.png"
-                mColorFilterList.add(ColorFilter(name, icon))
+                mColorFilterList.add(ColorFilter(name, icon, path))
             }
             promise.resolve(GsonBuilder().create().toJson(mColorFilterList))
         }
+
+        /**
+         * 获取录制滤镜
+         */
+        fun getRecordColorFilter(mContext: Context, promise: Promise) {
+            val mColorFilterList: MutableList<ColorFilter> = ArrayList()
+            mColorFilterList.add(ColorFilter("无效果", "ic_color_filter_empty", ""))
+            RecordCommon.getColorFilterList(mContext).forEach { path ->
+                val name = File(path).name
+                val icon = "file://$path/icon.png"
+                mColorFilterList.add(ColorFilter(name, icon, path))
+            }
+            Log.e("AAA", "mColorFilterList:" + mColorFilterList.size)
+//            val obj = Arguments.fromList(mColorFilterList)
+//            promise.resolve(obj)
+            promise.resolve(GsonBuilder().create().toJson(mColorFilterList))
+        }
     }
-
-//    private val mColorFilterList: MutableList<ColorFilter> = ArrayList()
-
 
     fun setColorFilter(filterName: String?, mAliyunIEditor: AliyunIEditor?) {
         if (filterName != null && filterName == "无效果") {
@@ -57,5 +75,5 @@ class ColorFilterManager(private val reactContext: ThemedReactContext) {
     }
 
 
-    internal class ColorFilter(var filterName: String, var iconPath: String)
+    internal class ColorFilter(var filterName: String, var iconPath: String, var path: String)
 }
