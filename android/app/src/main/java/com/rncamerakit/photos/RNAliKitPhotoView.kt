2dd49a3d6846b14d66d859e67a47b3d8112e7069
wuyq <wuyq08@icloud.com>
2022-05-16 11:11:28 +0800

    POST 相册RN 部分调整

--- android/app/src/main/java/com/rncamerakit/photos/RNAliKitPhotoView.kt
+++ android/app/src/main/java/com/rncamerakit/photos/RNAliKitPhotoView.kt
@@ -1,15 +1,18 @@
 package com.rncamerakit.photos
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.FrameLayout
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.aliyun.svideo.common.utils.FileUtils
 import com.aliyun.svideo.common.utils.ScreenUtils
 import com.aliyun.svideo.media.MediaInfo
 import com.duanqu.transcode.NativeParser
 import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.LifecycleEventListener
 import com.facebook.react.bridge.WritableMap
 import com.facebook.react.bridge.WritableNativeArray
 import com.facebook.react.uimanager.ThemedReactContext
@@ -24,15 +27,26 @@ class RNAliKitPhotoView(val reactContext: ThemedReactContext) : FrameLayout(reac
     private var mWidth = 0
     private var mHeight = 0
     private var mNumColumns = 4
-    private var mMultiSelect: Boolean = true
+    private var mInitViewLoad: Boolean = false
+    private var mMultiSelect: Boolean = false
     private var mPhotoAdapter: PhotoAdapter? = null
     private var mPhotoRecyclerView: RecyclerView? = null
     private var mPhotoList: MutableList<MediaInfo> = ArrayList()
 
+    private var mCurrentClickPosition = 0
+    private var mSelectedPhotoList: MutableList<MediaInfo> = ArrayList()
+
+    /**
+     * key 选中图片在相册的下标，
+     * value 选中图片在选中数组中的下标
+     */
+    private var mSelectedPhotoMap: HashMap<Int, Int> = HashMap()
+
     private var mEventEmitter: RCTEventEmitter? = null
 
     enum class EventEmitterKeys(private val mName: String) {
-        EVENT_SELECTED_PHOTOS("onSelectedPhotos");
+        EVENT_SELECTED_PHOTO_CALLBACK("onSelectedPhotoCallback"),
+        EVENT_MAX_SELECT_COUNT_CALLBACK("onMaxSelectCountCallback");
 
         override fun toString(): String {
             return mName
@@ -40,15 +54,33 @@ class RNAliKitPhotoView(val reactContext: ThemedReactContext) : FrameLayout(reac
     }
 
     init {
+        mInitViewLoad = false
         mEventEmitter = reactContext.getJSModule(RCTEventEmitter::class.java)
         this.mWidth = ScreenUtils.getWidth(reactContext)
         this.mHeight = ScreenUtils.getHeight(reactContext)
         mPhotoList.clear()
+        mSelectedPhotoList.clear()
 
         reactContext.runOnUiQueueThread {
             initViews()
             initMedias()
         }
+
+        reactContext.addLifecycleEventListener(object : LifecycleEventListener {
+            override fun onHostResume() {
+                //TODO 这里做刷新相册
+                if (mInitViewLoad) {
+//                    QueryMediaManager.instance.initLoad(mContext)
+                }
+            }
+
+            override fun onHostPause() {
+            }
+
+            override fun onHostDestroy() {
+            }
+
+        })
     }
 
     fun setPageSize(pageSize: Int) {
@@ -60,6 +92,23 @@ class RNAliKitPhotoView(val reactContext: ThemedReactContext) : FrameLayout(reac
 
     fun setMultiSelect(multiSelect: Boolean) {
         this.mMultiSelect = multiSelect
+
+        if (mInitViewLoad) {
+            if (multiSelect) {
+                //单选设置成多选，将当前选择的图片设置成选中
+                mSelectedPhotoList.clear()
+                mSelectedPhotoList.add(mPhotoList[mCurrentClickPosition])
+                mSelectedPhotoMap[mCurrentClickPosition] = mSelectedPhotoList.size
+            } else {
+                //多选设置成单选，将
+                mSelectedPhotoMap.clear()
+                mSelectedPhotoList.clear()
+                mSelectedPhotoList.add(mPhotoList[mCurrentClickPosition])
+            }
+
+            mPhotoAdapter?.setMultiSelect(multiSelect)
+            mPhotoAdapter?.notifyItemRangeChanged(0, mPhotoList.size, "MultiSelectChanged")
+        }
     }
 
     fun setItemWidth(itemWidth: Int) {
@@ -74,25 +123,85 @@ class RNAliKitPhotoView(val reactContext: ThemedReactContext) : FrameLayout(reac
         filAdapter()
 
         mPhotoAdapter?.setOnPhotoItemListener(object : PhotoAdapter.OnPhotoItemListener() {
-            override fun onItemClick(position: Int, info: MediaInfo) {
-                super.onItemClick(position, info)
+            override fun onAddPhotoClick(position: Int, info: MediaInfo) {
+                super.onAddPhotoClick(position, info)
+                mCurrentClickPosition = position
                 if (mMultiSelect) {
-                    val photoList: MutableList<MediaInfo> = ArrayList()
-                    photoList.add(info)
-                    sendRNSelectedPhotos(0, photoList)
+                    if (mSelectedPhotoMap[position] == null) {
+                        //多选
+                        mSelectedPhotoList.add(info)
+                        mSelectedPhotoMap[position] = mSelectedPhotoList.size
+                    }
+                    val selectPosition: Int? = mSelectedPhotoMap[position]
+                    selectPosition?.let { sendRNSelectedPhotos(it - 1, mSelectedPhotoList) }
+                } else {
+                    //单选
+                    mSelectedPhotoList.clear()
+                    mSelectedPhotoList.add(info)
+                    sendRNSelectedPhotos(0, mSelectedPhotoList)
+                }
+                Log.e("AAA", "mCurrentClickPosition：$mCurrentClickPosition")
+            }
+
+            override fun onRemovePhotoClick(position: Int, info: MediaInfo) {
+                super.onRemovePhotoClick(position, info)
+                val removeListPosition = mSelectedPhotoMap[position]
+                removeListPosition?.let {
+                    mSelectedPhotoList.removeAt(it - 1)
+                    mSelectedPhotoMap.remove(position)
+
+                    var maxValue: Int = 0
+                    var maxKey: Int = 0
+                    for ((key, value) in mSelectedPhotoMap) {
+                        if (value > removeListPosition) {
+                            mSelectedPhotoMap[key] = value - 1
+                        }
+                        mPhotoAdapter?.notifyItemChanged(key)
+
+                        if (value > maxValue) {
+                            maxValue = value
+                            maxKey = key
+                        }
+                        Log.e("AAA", "key:$key" + "；value:" + mSelectedPhotoMap[key])
+                    }
+
+                    if (position == mCurrentClickPosition) {
+                        mCurrentClickPosition = maxKey
+                    }
+
+                    if (mSelectedPhotoMap.isEmpty()) {
+                        mPhotoAdapter?.notifyItemRangeChanged(0, mPhotoList.size, "MultiSelectChanged")
+                    } else {
+                        mPhotoAdapter?.notifyItemChanged(position)
+                    }
+
+                    if (mSelectedPhotoList.isNotEmpty()) {
+                        mPhotoAdapter?.setCurrentClickPosition(mCurrentClickPosition)
+                    }
+
+                    Log.e("AAA", "mCurrentClickPosition：$mCurrentClickPosition")
+                    sendRNSelectedPhotos(mCurrentClickPosition, mSelectedPhotoList)
                 }
             }
+
+            override fun onMaxSelectCountCallback() {
+                super.onMaxSelectCountCallback()
+                mEventEmitter?.receiveEvent(id, EventEmitterKeys.EVENT_MAX_SELECT_COUNT_CALLBACK.toString(), Arguments.createMap())
+            }
         })
+
+        mInitViewLoad = true
     }
 
 
     private fun filAdapter() {
         if (mPhotoAdapter == null) {
-            mPhotoAdapter = PhotoAdapter(context, mPhotoList, this.mWidth/mNumColumns, this.mWidth/mNumColumns)
+            mPhotoAdapter = PhotoAdapter(context, mPhotoList, mSelectedPhotoMap, this.mWidth/mNumColumns, this.mWidth/mNumColumns)
             mPhotoAdapter?.setMultiSelect(mMultiSelect)
             mPhotoRecyclerView?.layoutManager = GridLayoutManager(context, mNumColumns)
             mPhotoRecyclerView?.addItemDecoration(GridSpacingItemDecoration(mNumColumns, 2, false))
-            mPhotoRecyclerView?.itemAnimator = DefaultItemAnimator()
+//            mPhotoRecyclerView?.itemAnimator = DefaultItemAnimator()
+            (mPhotoRecyclerView?.itemAnimator as DefaultItemAnimator).supportsChangeAnimations = false
             mPhotoRecyclerView?.adapter = mPhotoAdapter
         } else {
             mPhotoAdapter?.setMultiSelect(mMultiSelect)
@@ -146,18 +255,27 @@ class RNAliKitPhotoView(val reactContext: ThemedReactContext) : FrameLayout(reac
             val info = photoList[i]
             val filePath = info.filePath
 
+            if (!FileUtils.fileIsExists(filePath)) {
+                continue
+            }
+
             val videoFile = File(filePath)
             val nativeParser = NativeParser()
             nativeParser.init(filePath)
             val rotation = nativeParser.getValue(NativeParser.VIDEO_ROTATION).toInt()
-            val videoWidth = nativeParser.getValue(NativeParser.VIDEO_WIDTH).toDouble()
-            val videoHeight = nativeParser.getValue(NativeParser.VIDEO_HEIGHT).toDouble()
+            var videoWidth = nativeParser.getValue(NativeParser.VIDEO_WIDTH).toDouble()
+            var videoHeight = nativeParser.getValue(NativeParser.VIDEO_HEIGHT).toDouble()
+            if (rotation == 90 || rotation == 270) {
+                val temp = videoHeight
+                videoHeight = videoWidth
+                videoWidth = temp
+            }
 
             val map: WritableMap = Arguments.createMap()
             map.putInt("index", i)
             map.putInt("width", videoWidth.toInt())
             map.putInt("height", videoHeight.toInt())
-            map.putString("url", info.fileUri)
+            map.putString("url", "file://" + info.filePath)
             map.putDouble("fileSize", videoFile.length().toDouble())
             map.putString("filename", videoFile.name)
             map.putString("type", info.mimeType)
@@ -168,9 +286,9 @@ class RNAliKitPhotoView(val reactContext: ThemedReactContext) : FrameLayout(reac
         }
 
         val map: WritableMap = Arguments.createMap()
-        map.putInt("selectPosition", selectPosition)
+        map.putInt("selectedIndex", selectPosition)
         map.putArray("data", arrayList)
-        mEventEmitter?.receiveEvent(id, EventEmitterKeys.EVENT_SELECTED_PHOTOS.toString(), map)
+        mEventEmitter?.receiveEvent(id, EventEmitterKeys.EVENT_SELECTED_PHOTO_CALLBACK.toString(), map)
     }
 
     fun onDestroy() {
