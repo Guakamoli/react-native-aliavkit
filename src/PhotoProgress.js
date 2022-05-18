04623b0169322be18f8725d31d1d6de698cef1c8
butters <mhk466@163.com>
2022-05-18 17:39:55 +0800

    RN-照片多进度条组件-优化代码

--- src/PhotoProgress.js
+++ src/PhotoProgress.js
@@ -16,55 +16,41 @@ export default class PhotoProgress extends Component {
         playAnimaton: PropTypes.bool,
     });
 
-    // //有缓存不方便调试
-    // static defaultProps = ({
-    //     itemCount: 3,
-    //     itemDuration: 3,
-    //     currentDuration: 3,
-    //     playAnimaton: true,
-    // });
-
     render() {
         const {
             itemCount = 3,
             itemDuration = 3,
-            currentDuration = 5,
+            currentDuration = 4,
             playAnimaton = true
         } = this.props;
-        console.log("test - 0");
         //拼接一个数组用于渲染,元素起始时间大于当前时间时,显示为灰色进度条
         const progressData = Array.from({ length: itemCount }, (v, index) => {
             let start = index * itemDuration;
             let end = start + itemDuration;
-            let blank = start > currentDuration;
-            let cur = start <= currentDuration && end > currentDuration;
+            let next = end > currentDuration;
             let progress = Math.max(0, (currentDuration - start) / itemDuration);
             let width = new Animated.Value(progress);
-            if ((blank || cur) && playAnimaton) {
+            if (next && playAnimaton) {
                 let animatedData = {
                     toValue: 1,
                     duration: itemDuration * (1 - progress) * 1000,
                     easing: Easing.easeInOut,
-                    delay: Math.max(0, start - currentDuration)*1000,
+                    delay: Math.max(0, start - currentDuration) * 1000,
                     useNativeDriver: false,
                 };
                 Animated.timing(width, animatedData).start();
             }
             return {
-                start,
-                end,
-                blank,
                 key: 'progress_' + index,
-                cur,
-                progress,
+                next,
                 width,
             }
         });
         return (
             <View style={styles.rootView} >
                 {progressData.map(p => {
-                    return <View style={[styles.progress, p.blank && styles.blank, p.cur && styles.blank]} key={p.key} >
-                        <Animated.View style={[(p.blank||p.cur) && styles.animated, {
+                    return <View style={[styles.progress, p.next && styles.blank]} key={p.key} >
+                        <Animated.View style={[p.next && styles.animated, {
                             width: p.width.interpolate({
                                 inputRange: [0, 1],
                                 outputRange: ['0%', '100%'],
@@ -96,7 +82,7 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         borderRadius: 1.5,
         height: 3,
-        overflow:'hidden',
+        overflow: 'hidden',
     },
     blank: {
         backgroundColor: 'rgba(255, 255, 255, 0.4)'
