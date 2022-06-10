d3d595c90d88a31d337edcca113ba2e497536fdd
Wang Wenlin <sopl.wang@gmail.com>
2022-06-10 15:23:21 +0800

    feat: Revo iOS new theme landed (#248)

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,211 +1,64 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Button, SafeAreaView, StatusBar } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import CameraScreenExample from './CameraScreenExample';
+// import BarcodeScreenExample from './BarcodeScreenExample';
+import PostPickerExample from './PostPickerExample';
+import StoryPickerExample from './StoryPickerExample';
 
-import CameraScreenExample from './CameraScreenExample';
-import CameraExample from './CameraExample';
-import VideoEditorExample from './VideoEditorExample';
-import Example from './trimmer';
-import StoryMusic from '../../src/StoryMusic';
+type State = {
+  // example?: CameraExample | CameraScreenExample | BarcodeScreenExample;
+  example?: PostPickerExample | StoryPickerExample;
+}
 
-import CameraScreen from '../../src/CameraScreen';
-import PostUpload from '../../src/PostScreen';
-import PostEditor from '../../src/PostEditor';
-import Entry from '../../src/Entry'
-import store from '../../src/store';
-import { Provider } from 'react-redux';
+export default class App extends Component {
+  state: State;
 
-import ImageMap from '../../images/index';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+  constructor(props) {
+    super(props);
+    this.state = {
+      example: undefined,
+    };
+  }
 
-function HomeScreen(props) {
-  const { server, user, item, navigation, initType } = props;
-  const insets = useSafeAreaInsets()
-  const sendfile = async (data) => {
-    try {
-
-    } catch (e) {
-
+  render() {
+    if (this.state.example) {
+      const Example = this.state.example;
+      return <Example />;
     }
-
-  }
-  const goBack = () => {
-    navigation.goBack()
-
-  }
-  return (
-    <>
-      <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
-        <View style={{ backgroundColor: '#000' }}>
-          <Entry {...props} goBack={goBack} {...ImageMap} sendfile={sendfile} isExample={true}
-            insets={insets}
-            getUploadFile={(data) => {
-              // navigation.navigate('FeedsPublishView', { 'attachments': data, type: data[0].type.split('/')[0], })
-            }}></Entry>
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={styles.headerContainer}>
+          <Text style={{ fontSize: 60 }}>🎈</Text>
+          <Text style={styles.headerText}>
+            React Native Camera Kit
+          </Text>
         </View>
-      </SafeAreaView>
-    </>
-    // </View>
-  );
-  return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.headerContainer}>
-        <Text style={{ fontSize: 60 }}>🎈</Text>
-        <Text style={styles.headerText}>React Native Camera Kit</Text>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={() => this.setState({ example: PostPickerExample })}>
+            <Text style={styles.buttonText}>
+              Post Picker
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => this.setState({ example: StoryPickerExample })}>
+            <Text style={styles.buttonText}>
+              Story Picker
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => this.setState({ example: PostPickerExample })}>
+            <Text style={styles.buttonText}>
+              Picker
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('storyPost')}>
-          <Text style={styles.buttonText}>storyUpload</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('postUpload')}>
-          <Text style={styles.buttonText}>postUpload</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-}
-
-function Camera({ navigation }) {
-  return <CameraExample />;
-}
-
-function VideoEditor({ navigation }) {
-  React.useLayoutEffect(() => {
-    navigation.setOptions({
-      headerTitle: 'Camera',
-      headerRight: () => <Button title='Home' onPress={() => navigation.navigate('Home')} />,
-    });
-  }, [navigation]);
-
-  return <VideoEditorExample />;
-}
-
-function Story(props) {
-  return (
-    <CameraScreen
-      {...props}
-      // 退出操作
-      goback={() => {
-
-        props.navigation.navigate('Home');
-      }}
-      // 拿到上传数据
-      getUploadFile={(data) => {
-
-      }}
-      // story 跳转post 路由
-      goPost={() => {
-        props.navigation.push('postUpload');
-      }}
-      // 1
-      cameraFlipImage={require('../images/cameraFlipIcon.png')}
-      captureButtonImage={require('../images/cameraButton.png')}
-      torchOnImage={require('../images/torchOn.png')}
-      torchOffImage={require('../images/torchOff.png')}
-      closeImage={require('../images/close.png')}
-      musicImage={require('../images/music.png')}
-      beautifyImage={require('../images/beautify.png')}
-      beautyAdjustImag={require('../images/beautyAdjust.png')}
-      AaImage={require('../images/Aa.png')}
-      filterImage={require('../images/filter.png')}
-      musicRevampImage={require('../images/musicRevamp.png')}
-      videomusicIcon={require('../images/videomusicIcon.png')}
-      giveUpImage={require('../images/giveUp.png')}
-      tailorImage={require('../images/tailor.png')}
-      noVolumeImage={require('../images/noVolume.png')}
-      volumeImage={require('../images/volume.png')}
-      musicDynamicGif={require('../images/musicDynamic.gif')}
-      musicIconPng={require('../images/musicIcon.png')}
-      musicIcongray={require('../images/musicIcongray.png')}
-      musicSearch={require('../images/musicSearch.png')}
-      selectBeautify={require('../images/selectBeautify.png')}
-      noResultPng={require('../images/noResult.png')}
-      zoomMode='on'
-      showCapturedImageCount
-      cameraModule={true}
-    />
-  );
-}
-
-function Post(props) {
-  const { server, user, item, navigation } = props;
-  return (
-    <PostUpload
-      // refs={postRef}
-      {...props}
-      //  退出操作
-      goback={() => {
-        // 回退操作
-        props.navigation.replace('Home');
-      }}
-      goStory={() => {
-        props.navigation.replace('storyPost');
-      }}
-      goPostEditor={(data) => {
-
-        props.navigation.push('PostEditorBox', { ...data });
-      }}
-      // 拿到上传数据
-      getUploadFile={(data) => {
-
-      }}
-      multipleBtnImage={require('../images/multipleBtn.png')}
-      startMultipleBtnImage={require('../images/startMultipleBtn.png')}
-      postCameraImage={require('../images/postCamera.png')}
-      changeSizeImage={require('../images/changeSize.png')}
-      closePng={require('../images/close.png')}
-    />
-  );
-}
-
-function PostEditorBox(props) {
-  //
-
-  return (
-    <>
-      <PostEditor
-        {...props}
-        actions={{ rightButtonText: 'Done', leftButtonText: 'Cancel' }}
-        // 退出操作
-        // goback={
-        //   () => {
-
-        //     navigation.goBack(-1)
-        //   }
-
-        // }
-        goPostUpload={() => {
-          // navigation.push('')
-        }}
-        // 拿到上传数据
-        getUploadFile={(data) => {
-
-        }}
-        noVolumeImage={require('../images/noVolume.png')}
-        volumeImage={require('../images/volume.png')}
-        noResultPng={require('../images/noResult.png')}
-        cameraModule={true}
-      />
-    </>
-  );
-}
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-
-  return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name='Home' component={HomeScreen} options={{ headerShown: false }} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
